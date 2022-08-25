aac542b12d633c2997e401d9c2235926b3c05a7b
yousef653 <112089075+yousef653@users.noreply.github.com>
2022-08-25 17:21:03 +0430

    Update Problem #02.cpp

--- Problem #02.cpp
+++ Problem #02.cpp
@@ -1,3 +1,6 @@
+/*
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+*/
 #include<iostream>
 #include<conio.h>
 #define N 4000000
